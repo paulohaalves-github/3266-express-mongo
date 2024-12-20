b110033fa13af0d94d0a23e88e72363ee2c6a178
Paulo Alves <pauloalves.ti@outlook.com>
2024-12-20 20:15:42 -0300

    Busca por parâmetro

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -59,8 +59,18 @@ class LivroController {
         }        
     };
 
+    static async listarLivrosPorEditora (req, res) {
+        const editora = req.query.editora;
+        try {
+          const livrosPorEditora = await livro.find({ editora: editora });
+          res.status(200).json(livrosPorEditora);
+        } catch (erro) {
+          res.status(500).json({ message: `${erro.message} - falha na busca` });
+        }
+      }
 
 
+    
 }
 
 export default LivroController;
